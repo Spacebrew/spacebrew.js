f99903757570912d056174c86838a505a135f6a8
robotconscience <brett@robotconscience.com>
2012-10-24 10:12:46 -0400

    Added "bind" function for Safari + other browsers

--- library/sb.js
+++ library/sb.js
@@ -2,6 +2,31 @@
  * Utils
  */
 
+// add bind method for browsers that don't currently support it (such as Safari)
+if (!Function.prototype.bind) {  
+  Function.prototype.bind = function (oThis) {  
+	if (typeof this !== "function") {  
+	  // closest thing possible to the ECMAScript 5 internal IsCallable function  
+	  throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");  
+	} 
+  
+	var aArgs = Array.prototype.slice.call(arguments, 1),   
+		fToBind = this,   
+		fNOP = function () {},  
+		fBound = function () {  
+		  return fToBind.apply(this instanceof fNOP  
+								 ? this  
+								 : oThis || window,  
+							   aArgs.concat(Array.prototype.slice.call(arguments)));  
+		};  
+  
+	fNOP.prototype = this.prototype;  
+	fBound.prototype = new fNOP();  
+  
+	return fBound;  
+  };  
+} 
+
 if (!window['getQueryString']){
 	/**
 	 * Get parameters from a query string
