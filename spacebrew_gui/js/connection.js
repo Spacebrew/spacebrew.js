17808cc4c344593f32fa30914d5f3d3e4977f8f2
Quin Kennedy <quinkennedy@LAB-macbookpro-02.rockwellgroup.com>
2012-11-07 17:41:15 -0500

    updated descriptions

--- spacebrew_gui/js/connection.js
+++ spacebrew_gui/js/connection.js
@@ -8,7 +8,7 @@ function setup (){
   sb = new Spacebrew.Client(
           undefined, 
           getQueryString('name') || (getQueryString('id') ? ("SpaceGUI" + getQueryString('id')) : "Untitled SpaceGUI"),
-          "GUI for sending and displaying SpaceBrew values.");
+          "GUI for sending and displaying SpaceBrew range messages.");
   sb.addPublish("slider1", "range", "500");
   sb.addPublish("slider2", "range", "500");
   sb.addSubscribe("slider1", "range");
